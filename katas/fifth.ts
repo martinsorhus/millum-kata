c7f3e13233687abea8717def61a23d1bb16b17af
Martin <sorhus.martin@gmail.com>
2024-03-11 22:15:20 +0100

    add( fifth kata ): add empty kata + test file for fifth kata

---
+++ katas/fifth.ts
@@ -0,0 +1,15 @@
+/**
+ * Implement a function that takes in an array of users
+ * The function should create an object ( obj ) with one key for each unique first letter in the user names
+ * the value for each key should be an array of the name of the users that starts with the letter of the key
+ * all values in array should be sorted in reverse alphabetical order
+ * All keys should be lowercase, all values should be uppercase
+ *
+ */
+
+import { User } from "../interfaces/user.interface";
+
+export const fifth = (users: User[]) => {
+  let obj = {};
+  return obj;
+};
