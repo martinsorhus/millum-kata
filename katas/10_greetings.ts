79a35ee9824f39b46c8deb29a4294bbaf2f47085
Martin <sorhus.martin@gmail.com>
2025-03-23 17:15:18 +0100

    change( kata #10 greetings ): add test example of kata output

--- katas/10_greetings.ts
+++ katas/10_greetings.ts
@@ -1,6 +1,11 @@
 /**
  * Write a function that greets the name given in the input
  * The outer function will only take in the name as an input, but that does not mean that the greeting will be static
+ * 
+ *      Here is an example of how the function should work
+ *      const a = greetings('James');
+        const b = a('Hello');
+        expect(b).toBe('Hello, James');
  */
 
 export const greetings = (name: string): Function => {
